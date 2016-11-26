518256c265a90b94a71ecc819f10b669b8d8fa08
tamamo <liuyejunnan@163.com>
2016-11-26 19:47:08 +0800

    CSS lazy-load

---
+++ src/views/video.vue
@@ -0,0 +1,29 @@
+<style scoped lang="less">
+    @import '../less/video.less';
+</style>
+<template>
+    <section class="video">
+        <img src="../images/tamamo.jpg">
+        <i class = "tamamo-icon cloud">&#xe61a;</i>
+    </section>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+
+            }
+        },
+        ready () {
+
+        },
+        beforeDestroy () {
+
+        },
+        methods: {
+            console () {
+                console.log(2);
+            }
+        }
+    }
+</script>
