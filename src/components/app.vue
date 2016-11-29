9440d8b778bc9003f4b70382e21f8c813ac2bc24
lyjn <lyjn@sodao.com>
2016-11-29 17:53:12 +0800

    增加backTop

--- src/components/app.vue
+++ src/components/app.vue
@@ -7,18 +7,20 @@
         <el-foot></el-foot>
         <el-loading></el-loading>
         <el-meter></el-meter>
+        <el-back-top></el-back-top>
     </div>
 </template>
 <script>
     import elFoot from '../components/foot';
     import elLoading from '../components/loading';
     import elMeter from '../components/meter';
+    import elBackTop from '../components/backTop';
     import store from '../vuex/store.js';
     export default {
         name: 'tamamo',
         store,
         components: {
-            elFoot, elLoading, elMeter
+            elFoot, elLoading, elMeter, elBackTop
         },
         data () {
             return {
---
+++ src/components/backTop.vue
@@ -0,0 +1,49 @@
+<style scoped lang="less">
+    @import '../less/backTop.less';
+</style>
+<template>
+  <section :class="['go-top', {'back-top-show': topShow}]" @click="backTop">
+  	<svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-top"></use>
+    </svg>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      topShow: false,
+      speed: 0
+    };
+  },
+  methods: {
+    backTop() {
+      let that = this;
+      that.speed = (document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop) / 10;
+      window.requestAnimationFrame(this.stepBack);
+    },
+    stepBack() {
+      let that = this;
+      let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+      scrollTop -= that.speed;
+      window.scrollTo(0, scrollTop);
+      if (scrollTop > 0) {
+        window.requestAnimationFrame(this.stepBack);
+      }
+    }
+  },
+  ready() {
+    let that = this;
+    window.onscroll = () => {
+      let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+      let lenWin = window.screen.height;
+      if (scrollTop >= lenWin * 0.5) {
+        that.topShow = true;
+      } else {
+        that.topShow = false;
+      }
+      return;
+    };
+  }
+};
+</script>
