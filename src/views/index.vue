a9859925d5b02007c871902f7873ba39d660359f
lyjn <lyjn@sodao.com>
2016-12-02 16:05:30 +0800

    增加ajax获取JSON

--- src/views/index.vue
+++ src/views/index.vue
@@ -17,43 +17,22 @@
 		},
 		data () {
 			return {
-				bangumiData: [
-					{
-						"name": "伪恋",
-						"cover": "http://i0.hdslb.com/320_200/video/d0/d0b5808d7e4f16c00f93b8fc5153dabe.jpg",
-						"islike": false
-					},
-					{
-						"name": "白箱",
-						"cover": "http://i2.hdslb.com/320_200/video/dc/dcd38eac6fb993d02d2b7453c224eec7.jpg",
-						"islike": false
-					},
-					{
-						"name": "我的青春恋爱物语一定有问题",
-						"cover": "http://i0.hdslb.com/320_200/video/69/6948d3ac5b41cd4292532c3af1178656.jpg",
-						"islike": false
-					},
-					{
-						"name": "希德尼娅的骑士",
-						"cover": "http://i2.hdslb.com/320_200/video/a4/a4ff34b87a8e71409d69dcac35994044.jpg",
-						"islike": false
-					},
-					{
-						"name": "噬神者",
-						"cover": "http://i0.hdslb.com/bfs/archive/7cfcdcb922d430bc5e399f59437cf5a925347e6f.jpg_320x200.jpg",
-						"islike": false
-					},
-					{
-						"name": "战场女武神",
-						"cover": "http://i0.hdslb.com/320_200/video/54/54a9bb84e872d5fe7b5a317852506981.jpg",
-						"islike": false
-					}
-				]
+				bangumiData: []
 			}
 		},
 		mounted () {
+			this.getData();
 		},
 		methods: {
+			getData: function () {
+				axios.get('http://ohjq9c9az.bkt.clouddn.com/list.json').then((res) => {
+					if (res.data && res.status === 200) {
+						this.bangumiData = res.data;
+					}
+				}).catch( (error)=> {
+					console.log(error)
+				})
+			},
 			add: function () {
 				this.$store.dispatch('ADD_COUNT', 1)
 			}
